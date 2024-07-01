02c12701a7bae49c0ca3608fc3701f62b75ee9be
Moumouuu <robin.travail007@gmail.com>
2024-07-01 09:05:44 +0200

    feat(): add projects

--- src/components/pages/home/Projects.tsx
+++ src/components/pages/home/Projects.tsx
@@ -6,45 +6,43 @@ export default function Projects() {
     {
       name: "EduFiche",
       description:
-        "This project is a platform for students to share generate in 1 click a sheet to revise their lessons.",
+        "Une plateforme de génération de fiches de révision pour tous les étudiants en quelques clics. Avec plus de 4000 utilisateurs et 3000 fiches générées.",
       image: "/images/opengraph-edufiche.png",
       link: "https://edu-fiche.fr",
     },
     {
-      name: "EduFiche",
-      description: "This is project 1",
-      image: "/images/opengraph-edufiche.png",
-      link: "https://edu-fiche.fr",
+      name: "FeelWords",
+      description:
+        "Une application collaborative de partage d'histoire avec un espaces d'écriture boosté par l'IA. Avec des fonctionnalités de partage, de lecture, de commentaires et de tournois pour les plus compétitifs.",
+      image: "/images/feelwords.png",
+      link: "https://feelwords.fr",
     },
     {
-      name: "EduFiche",
-      description: "This is project 1",
-      image: "/images/opengraph-edufiche.png",
-      link: "https://edu-fiche.fr",
+      name: "Jalla Mulhouse",
+      description:
+        "Un site vitrine accompagné d'un espace administrateur, spécialiste du linge de maison depuis 2001.",
+      image: "/images/jalla-mulhouse.png",
+      link: "https://jalla-mulhouse.fr",
     },
     {
-      name: "EduFiche",
-      description: "This is project 1",
-      image: "/images/opengraph-edufiche.png",
-      link: "https://edu-fiche.fr",
+      name: "Juiher Nature",
+      description:
+        "Un site vitrine accompagné d'un espace administrateur, spécialisé dans l'élagage et l'abattage d'arbres.",
+      image: "/images/juiher-nature.png",
+      link: "https://juihernature.fr",
     },
     {
-      name: "EduFiche",
-      description: "This is project 1",
-      image: "/images/opengraph-edufiche.png",
-      link: "https://edu-fiche.fr",
-    },
-    {
-      name: "EduFiche",
-      description: "This is project 1",
-      image: "/images/opengraph-edufiche.png",
-      link: "https://edu-fiche.fr",
+      name: "BookEazy",
+      description:
+        "Une application qui permet aux entreprises de gérer leurs rendez-vous, et d'obtenir des statistiques sur leur activité et leurs clients.",
+      image: "/images/bookeazy.png",
+      link: "https://bookeazy.fr",
     },
   ];
   return (
     <section className=" bg-main border-y-2 border-y-darkBorder g py-20 font-base lg:py-[100px]">
       <h2 className="mb-14 px-5 text-center text-2xl font-heading md:text-3xl lg:mb-20 lg:text-4xl text-white">
-        Recent projects
+        Nos derniers projets
       </h2>
       <div className="flex flex-wrap justify-center">
         {projects.map((project) => (
